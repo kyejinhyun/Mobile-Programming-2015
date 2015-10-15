4ed0f6fab40f5167778d0ebf2ca930775962f5fe
KJH-NOTE <KJH-NOTE@KJH-NOTE-PC>
2015-10-15 12:55:52 +0900

    10.15 problem.1

---
+++ src/kr/ac/kookmin/exception/basicthread/testMain.java
@@ -0,0 +1,37 @@
+package kr.ac.kookmin.exception.basicthread;
+public class testMain {
+	
+	public static BankAccount account = new BankAccount();
+	public static void main (String []args) {
+		
+	
+		Thread depositMan = new Thread() {
+            public void run() {
+                while (true) {
+                	try {
+						sleep(500);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			account.add(10);// 10 만큼 입금
+            }
+            }
+        };
+        Thread withdrawMan = new Thread() {
+            public void run() {
+                while (true) {
+                        try {
+							sleep(500);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+            account.delete(10);// 10 만큼 출금               
+                        }
+            }
+        };
+        depositMan.start();
+        withdrawMan.start();
+	}
+}
